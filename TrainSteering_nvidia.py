85d51af881a3c19a94d518417dbdfb95310311fc
yingweiy <yingweiy@gmail.com>
2017-08-26 18:26:37 -0500

    car drive simulation with nvidia deep network

--- TrainSteering_nvidia.py
+++ TrainSteering_nvidia.py
@@ -9,16 +9,26 @@ from keras.layers import MaxPool2D
 
 
 def loadImage(log, att, correction=0,  flip=False, measure_att='SteeringAngle'):
+    print(att, flip)
     nsamples = log[att].count()
     for i in tqdm(range(nsamples)):
         image = cv2.imread(log[att].values[i])
+        if image is None:
+            continue
         if flip==True:
-            np.fliplr(image)
+            image = np.fliplr(image)
         images.append(image)
         measure = log[measure_att].values[i] + correction
         if flip == True:
             measure = -measure
         measurements.append(measure)
+
+
+#fn = '/home/yingweiy/SDC/udacity_code/Project3_Data/IMG/center_2017_08_26_17_01_33_576.jpg'
+#image = cv2.imread(fn)
+#image = np.fliplr(image)
+#exit()
+
 
 lines = []
 csvfn = '/home/yingweiy/SDC/udacity_code/Project3_Data/driving_log.csv'
@@ -30,9 +40,11 @@ log = pd.read_csv(csvfn, names = ['CenterImage', 'LeftImage', 'RightImage', 'Ste
 images = []
 measurements = []
 loadImage(log, 'CenterImage')
+
 augmentation = True
 if augmentation == True:
     loadImage(log, 'CenterImage', flip=True)
+
 loadImage(log, 'LeftImage', correction=0.2)
 loadImage(log, 'RightImage', correction=-0.2)
 
@@ -42,20 +54,22 @@ y_train = np.array(measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((75,25), (0,0))))
-model.add(Convolution2D(6,(5,5),activation='relu'))
-model.add(MaxPool2D())
-model.add(Convolution2D(6,(5,5),activation='relu'))
-model.add(MaxPool2D())
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24,(5,5), strides=(2,2), activation='relu'))
+model.add(Convolution2D(36,(5,5), strides=(2,2), activation='relu'))
+model.add(Convolution2D(48,(5,5), strides=(2,2), activation='relu'))
+model.add(Convolution2D(64,(3,3), activation='relu'))
+model.add(Convolution2D(64,(3,3), activation='relu'))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(80))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
 
-model.save('m1.h5')
+model.save('m_nv1.h5')
 print('Done.')
 
 
